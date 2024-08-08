63c1851d1db36239888a43ddfb13445aa9712fde
Gaba31 <brijeshgaba99@gmail.com>
2024-08-09 00:32:28 +0530

    Creating src file and pushing all js code in it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const Header = ()=>{
-    return (
-        <div className="header">
-            <div className="logo-container">
-            <img className="logo" src="https://img.freepik.com/free-vector/hand-drawn-spicy-logo-design_23-2149667267.jpg?t=st=1723049243~exp=1723052843~hmac=c885d9a30eee4fc0e5aea7877200b13f98b88f47faeb6610bda3fa23a73c5e06&w=740"></img>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    );
-}
+import Header from "./components/Header";
 
 const resList =  [
     {
